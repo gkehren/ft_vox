576bf166bcc302625e89891d59459762ab112723
gkehren <kehren.guillaume@gmail.com>
2024-04-30 19:11:09 +0200

    ThreadPool & other opti

--- src/Engine/Engine.hpp
+++ src/Engine/Engine.hpp
@@ -24,6 +24,7 @@
 #include <Camera/Camera.hpp>
 #include <Renderer/TextRenderer.hpp>
 #include <utils.hpp>
+#include <Engine/ThreadPool.hpp>
 
 
 class Engine {
@@ -46,13 +47,16 @@ class Engine {
 		float					lastTime;
 		float					fps;
 		bool					paused;
+		bool					perfMode;
 
 		Shader*					shader;
 		Renderer*				renderer;
 		TextRenderer*			textRenderer;
 		Camera					camera;
-
+		ThreadPool*				threadPool;
+		std::mutex				mutex;
 		glm::ivec2				playerChunkPos;
+
 
 		void	handleInput(bool& keyTPressed);
 		void	updateUI();
@@ -67,6 +71,7 @@ class Engine {
 		TextureType	selectedTexture;
 		siv::PerlinNoise*	perlin;
 		std::unordered_map<glm::ivec3, Chunk, ivec3_hash>	chunks;
+		std::vector<glm::ivec3>								chunksToCreate;
 
 		void	render();
 		void	frustumCulling();
