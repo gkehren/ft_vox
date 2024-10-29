7e66247b3f78302eca947be16a4ca09273f1ccfd
gkehren <kehren.guillaume@gmail.com>
2024-10-29 21:34:16 +0100

    Add CMakeLists.txt for windows

--- src/Engine/Engine.cpp
+++ src/Engine/Engine.cpp
@@ -9,7 +9,7 @@ Engine::Engine()
 	}
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	#ifdef __APPLE__
 		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
