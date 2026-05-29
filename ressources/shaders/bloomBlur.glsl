e2ba9d95375e584d054ce736e3506ae1e69d877f
gkehren <kehren.guillaume@gmail.com>
2026-05-29 23:55:29 +0200

    Optimize: Bilinear 5-tap Gaussian Bloom Blur (reducing texture samples from 9 to 5)

--- ressources/shaders/bloomBlur.glsl
+++ ressources/shaders/bloomBlur.glsl
@@ -7,8 +7,9 @@ uniform sampler2D image;
 uniform bool horizontal;
 uniform vec2 texelSize;
 
-// 9-tap Gaussian weights (sigma ~= 4.0)
-const float weights[5] = float[](0.227027, 0.1945946, 0.1216216, 0.054054, 0.016216);
+// Bilinear offsets and weights for 9-tap equivalent using 5 taps
+const float offsets[3] = float[](0.0, 1.38461538, 3.23076923);
+const float weights[3] = float[](0.22702702, 0.31621622, 0.07027027);
 
 void main()
 {
@@ -16,18 +17,18 @@ void main()
 
     if (horizontal)
     {
-        for (int i = 1; i < 5; ++i)
+        for (int i = 1; i < 3; ++i)
         {
-            result += texture(image, TexCoord + vec2(texelSize.x * i, 0.0)).rgb * weights[i];
-            result += texture(image, TexCoord - vec2(texelSize.x * i, 0.0)).rgb * weights[i];
+            result += texture(image, TexCoord + vec2(texelSize.x * offsets[i], 0.0)).rgb * weights[i];
+            result += texture(image, TexCoord - vec2(texelSize.x * offsets[i], 0.0)).rgb * weights[i];
         }
     }
     else
     {
-        for (int i = 1; i < 5; ++i)
+        for (int i = 1; i < 3; ++i)
         {
-            result += texture(image, TexCoord + vec2(0.0, texelSize.y * i)).rgb * weights[i];
-            result += texture(image, TexCoord - vec2(0.0, texelSize.y * i)).rgb * weights[i];
+            result += texture(image, TexCoord + vec2(0.0, texelSize.y * offsets[i])).rgb * weights[i];
+            result += texture(image, TexCoord - vec2(0.0, texelSize.y * offsets[i])).rgb * weights[i];
         }
     }
 
